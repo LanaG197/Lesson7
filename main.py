5e6903937f676a3e76ec4087cfa69595c9d5e36b
LanaG197 <svetlychok73@mail.ru>
2024-09-11 22:07:04 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
+# Создать переменную name, присвоить значение имени, вывести на экран
 name = "Svetlana"
 print("Name:", name)
+# Создать переменную age, присвоить значение возраста (целое число), вывести на экран
 age = 51
 print("Age:", age)
-new_age = 35
-print("New Age:", new_age)
+# Перезаписать в age текущее значение переменной age + новое, вывести на экран
+age = 51
+print('age')
+age = age - 16
+print("New Age", "age")
+# Создать переменную is_student, присвоить значение True (логическое значение), вывести на экран
 is_student = True
 print("Is Student:", is_student)
